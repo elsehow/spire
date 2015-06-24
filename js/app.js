4b4a584ad117000b47abd15a5e9f6e42937296c0
Noura <culurienneldoreth@gmail.com>
2015-06-23 17:55:21 -0700

    trying to add spire to hellojs... abandoning this attempt for now

--- js/app.js
+++ js/app.js
@@ -7,15 +7,46 @@ var app = (function() {
     var access_token = '5b55104c38593937e45252cec198570e0e65c83bbd24278c6f64acf8a2aace76';
 
     function init() {
-        hello.init({
-            facebook: '1443430279299971',
-        }, {redirect_uri: 'index.html'});
+        
+        hello.init(
+            {
+                spire: {
+                    name: 'Spire',
+                    oauth: {
+                        version: 2,
+                        auth: 'https://app.spire.io/dialog/oauth/',
+                        grant: 'https://app.spire.io/oauth/access_token', 
+                    },
+                    base: 'https://app.spire.io',
+                },
+            }, 
+            {
+                redirect_uri: 'index.html'
+            }
+        );
+
+        hello('spire').login();
+
+        hello.on('auth.login', function(auth) {
+            console.log('logged in to spire? auth', auth);
+        });        
+
+        /*
+        hello.init(
+            {
+                facebook: '1443430279299971',
+            },
+            {
+                redirect_uri: 'index.html'
+            }
+        );
 
         hello.on('auth.login', function(auth) {
             $('#login').hide();
             $('#login-successful').show();
             console.log('auth', auth);
         });
+        */
     }
 
     return {
