bb41ac423519cbcc42d77753b52f1200db76324c
Noura <culurienneldoreth@gmail.com>
2015-06-23 21:45:37 -0700

    added basic plot of breath data

--- client/js/app.js
+++ client/js/app.js
@@ -4,6 +4,7 @@ window.onload = function() {
 
 var spire_app = (function() {
     var data = null;
+    var graph = null;
 
     function init() {
         $.ajax({
@@ -16,10 +17,32 @@ var spire_app = (function() {
             },
             method: 'GET',
             success: function(res) {
-                var data = JSON.parse(res);
+                data = JSON.parse(res);
                 console.log('got response', data);
+                init_graph();
             }
         });
+    }
+
+    function init_graph() {
+        var formatted_data = _.map(data.data, function(o) {
+            return { x: o.timestamp, y: o.value };
+        });
+        console.log('formatted_data', formatted_data);
+        graph = new Rickshaw.Graph({
+            element: document.querySelector('#chart'),
+            width: $(window).width() - 80, // TODO magic numbers are bad
+            height: $(window).height() - 80,
+            series: [{ color: 'steelblue', data: formatted_data }],
+        });
+        var axes = new Rickshaw.Graph.Axis.Time({graph: graph});
+        var y_axis = new Rickshaw.Graph.Axis.Y({
+            graph: graph,
+            orientation: 'left',
+            tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+            element: document.getElementById('y_axis'),
+        });
+        graph.render();
     }
 
     return {
