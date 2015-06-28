31a1800faa215c8a70f9966f2dfe451d7049b8ab
Noura <culurienneldoreth@gmail.com>
2015-06-28 13:52:58 -0700

    the 'all' button gets ALL data. it takes a while

--- client/js/app.js
+++ client/js/app.js
@@ -30,7 +30,7 @@ var spire_app = (function() {
                     name: 'breaths per minute',
                 }
             ],
-            renderer: 'line',
+            renderer: 'scatterplot',
         });
         var y_axis = new Rickshaw.Graph.Axis.Y({
             graph: graph,
@@ -55,6 +55,11 @@ var spire_app = (function() {
         graph.update();
     }
 
+    function reset_breath_data() {
+        raw_breath_data = [];
+        processed_breath_data = [];
+    }
+
     function update_breath_data(d) {
         raw_breath_data.push.apply(raw_breath_data, d);
         _.sortBy(raw_breath_data, function (o) {
@@ -68,32 +73,68 @@ var spire_app = (function() {
         _.sortBy(processed_breath_data, function(o) {
             return o.x;
         });
-
-        update_graph();
     }
 
     function get_data(from, to) {
         console.log('requesting data from', from, 'to', to);
-        $.ajax({
+        var spire_query = 'https://app.spire.io//api/events/br?';
+        if ( from !== undefined && to !== undefined) {
+            spire_query += 'from=' + from + '&to=' + to;
+        }
+        return $.ajax({
             url: 'http://localhost:3000',
             data: {
-                url: 'https://app.spire.io//api/events/br?from=' + from + '&to=' + to,
+                url: spire_query,
             },
             error: function(jqxhr, textStatus, errorThrown) {
                 console.log('error in ajax request for spire breath data', jqxhr, textStatus, errorThrown);
             },
             method: 'GET',
-            success: function(res) {
-                console.log('got back res:', res);
-                try {
-                    data = JSON.parse(res);
-                    console.log('got data', data);
-                    update_breath_data(data.data);
-                } catch (e) {
-                    console.log('error e', e);
-                }
+        });
+    }
+
+    function get_all_data() {
+        var t = null;
+        var d = [];
+        var n_received = null;
+        var n_expected = null;
+        get_data().then(function(res) {
+            var data = JSON.parse(res);
+            update_breath_data(data.data);
+            update_graph();
+            t = data.data.slice(-1)[0].timestamp;
+            console.log('initial time', t, 'with', data.data.length, 'data points');
+            d.push.apply(d, data.data);
+            var ts = [];
+            while (t * 1000 < Date.now()) {
+                ts.push(t);
+                t += 1000;
+            }
+            n_expected = ts.length;
+            console.log('ts', ts);
+            for (var i = 0; i < ts.length; i++) {
+                console.log('requesting data for time', ts[i]);
+                get_data(ts[i], ts[i] + 1000).then(function(res) {
+                    n_received += 1;
+                    var data = JSON.parse(res);
+                    console.log('from time', data.data.slice(-1)[0].timestamp, 'with', data.data.length, 'data points');
+                    d.push.apply(d, data.data);
+                });
             }
         });
+        function when_done_hack() {
+            console.log('when done hack n_received', n_received, 'n_expected', n_expected);
+            if (n_received === null ||
+                n_expected === null ||
+                n_received < n_expected) {
+                setTimeout(when_done_hack, 5000);
+            } else {
+                console.log('\t DONE');
+                update_breath_data(d);
+                update_graph();
+            }
+        }
+        when_done_hack();
     }
 
     function init() {
@@ -101,7 +142,22 @@ var spire_app = (function() {
         $('#submit').on('click', function(e) {
             var from = Math.floor(new Date($('#from').val()).getTime() / 1000) - tzOffset;
             var to = Math.floor(new Date($('#to').val()).getTime() / 1000) - tzOffset;
-            get_data(from, to);
+            get_data(from, to).then(function(res) {
+                console.log('got back res:', res);
+                try {
+                    data = JSON.parse(res);
+                    console.log('got data', data);
+                    reset_graph();
+                    reset_breath_data();
+                    update_breath_data(data.data);
+                    update_graph();
+                } catch (e) {
+                    console.log('error e', e);
+                }
+            });
+        });
+        $('#all').on('click', function(e) {
+            get_all_data();
         });
     }
 
