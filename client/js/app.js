3b002c625c75fb5fd35ee39a640eb5c70f6777cf
Noura <culurienneldoreth@gmail.com>
2015-06-28 12:55:23 -0700

    refactoring

--- client/js/app.js
+++ client/js/app.js
@@ -3,18 +3,10 @@ window.onload = function() {
 };
 
 var spire_app = (function() {
-    var data = null;
+    var raw_breath_data = [];
+    var processed_breath_data = [];
     var graph = null;
     var tzOffset = -7 * 3600;
-
-    function init() {
-        $('#submit').on('click', function(e) {
-            var from = Math.floor(new Date($('#from').val()).getTime() / 1000) - tzOffset;
-            var to = Math.floor(new Date($('#to').val()).getTime() / 1000) - tzOffset;
-            console.log('from', from, 'to', to);
-            graph_data(from, to);
-        });
-    }
 
     function format_hr_min(date) {
         var hours = date.getHours().toString();
@@ -25,38 +17,8 @@ var spire_app = (function() {
         return hours + ':' + minutes;
     }
 
-    function graph_data(from, to) {
+    function reset_graph() {
         $('#chart_container').html('<div id="y_axis"></div><div id="chart"></div>');
-        $.ajax({
-            url: 'http://localhost:3000',
-            data: {
-                url: 'https://app.spire.io//api/events/br?from=' + from + '&to=' + to,
-            },
-            error: function(jqxhr, textStatus, errorThrown) {
-                console.log('error in ajax request for spire breath data', jqxhr, textStatus, errorThrown);
-            },
-            method: 'GET',
-            success: function(res) {
-                try {
-                    data = JSON.parse(res);
-                    if (data.data.length === 0) {
-                        alert('No data points for this time range');
-                    }
-                    console.log('got response', data);
-                    init_graph();
-                } catch (e) {
-                    console.log('error e', e);
-                    alert('Probably there is no data available for this time period.' + e);
-                }
-            }
-        });
-    }
-
-    function init_graph() {
-        var formatted_data = _.map(data.data, function(o) {
-            return { x: o.timestamp + tzOffset, y: o.value };
-        });
-        console.log('formatted_data', formatted_data);
         graph = new Rickshaw.Graph({
             element: document.querySelector('#chart'),
             width: $(window).width() - 80, // TODO magic numbers are bad
@@ -64,13 +26,12 @@ var spire_app = (function() {
             series: [
                 {
                     color: 'steelblue',
-                    data: formatted_data,
-                    name: 'breathing',
+                    data: processed_breath_data,
+                    name: 'breaths per minute',
                 }
             ],
             renderer: 'line',
         });
-        graph.render();
         var y_axis = new Rickshaw.Graph.Axis.Y({
             graph: graph,
             orientation: 'left',
@@ -87,6 +48,60 @@ var spire_app = (function() {
                 var d = new Date((x-tzOffset) * 1000);
                 return format_hr_min(d) + ' ' + d.toDateString();
             },
+        });
+    }
+
+    function update_graph() {
+        graph.update();
+    }
+
+    function update_breath_data(d) {
+        raw_breath_data.push.apply(raw_breath_data, d);
+        _.sortBy(raw_breath_data, function (o) {
+            return o.timestamp;
+        });
+
+        var new_processed_breath_data = _.map(d, function(o) {
+            return { x: o.timestamp + tzOffset, y: o.value };
+        });
+        processed_breath_data.push.apply(processed_breath_data, new_processed_breath_data);
+        _.sortBy(processed_breath_data, function(o) {
+            return o.x;
+        });
+
+        update_graph();
+    }
+
+    function get_data(from, to) {
+        console.log('requesting data from', from, 'to', to);
+        $.ajax({
+            url: 'http://localhost:3000',
+            data: {
+                url: 'https://app.spire.io//api/events/br?from=' + from + '&to=' + to,
+            },
+            error: function(jqxhr, textStatus, errorThrown) {
+                console.log('error in ajax request for spire breath data', jqxhr, textStatus, errorThrown);
+            },
+            method: 'GET',
+            success: function(res) {
+                console.log('got back res:', res);
+                try {
+                    data = JSON.parse(res);
+                    console.log('got data', data);
+                    update_breath_data(data.data);
+                } catch (e) {
+                    console.log('error e', e);
+                }
+            }
+        });
+    }
+
+    function init() {
+        reset_graph();
+        $('#submit').on('click', function(e) {
+            var from = Math.floor(new Date($('#from').val()).getTime() / 1000) - tzOffset;
+            var to = Math.floor(new Date($('#to').val()).getTime() / 1000) - tzOffset;
+            get_data(from, to);
         });
     }
 
