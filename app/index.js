8bfc4b975cd244bb22a585056d125e12e306874a
elsehow <yes@cosmopol.is>
2015-08-20 18:16:25 -0700

    added a rudementary 'tooltip' that shows what time the cursor is over

--- app/index.js
+++ app/index.js
@@ -1,34 +1,57 @@
 var $ = require('jquery')
-   , _ = require('lodash')
-   , Kefir = require('kefir')
-   , dateSelector = require('./src/dateSelector.js')
-   , spireAPI = require('./src/spireAPI.js')
-   , windowed = require('./src/windowed.js')
-   , BarGraph = require('./src/barGraph.js')
+  , Kefir = require('kefir')
+  , dateSelector = require('./src/dateSelector.js')
+  , spireAPI = require('./src/spireAPI.js')
+  , windowed = require('./src/windowed.js')
+  , barGraph = require('./src/barGraph.js')
+  , map = function (num, in_min, in_max, out_min, out_max) {
+      return (num- in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
 
 var setup = function() {
   //streams
-  dateSelectionStream = dateSelector()
-  spireDataStream = dateSelectionStream.flatMapLatest(spireAPI)
+  var dateSelectionStream = dateSelector()
+  var spireDataStream     = dateSelectionStream.flatMapLatest(spireAPI)
   //setup
-  drawGraph1 = BarGraph('graph1')
-  drawGraph2 = BarGraph('graph2')
-  drawGraph3 = BarGraph('graph3')
+  $(document.body).append('<div id = "graphsContainer"></div>')
+  var $graphsContainer = $('#graphsContainer')
   //side effects
-  dateSelectionStream.log('fetching') // 'loading spinner'
+  dateSelectionStream.onValue(function (date) {
+    $graphsContainer.html('loading ' + date)
+  })
   spireDataStream.onValue (function (d) {
-    // process data
     var timeseries = d['data']
+    // clear loading screen
+    $graphsContainer.empty()
+    // process data
     var w1 = windowed(timeseries, 50) // hi-res breath data
     var w2 = windowed(timeseries, 200) // mid-res breath data
     var w3 = windowed(timeseries, 500) // lo-res breath data
     // draw graph
-    drawGraph1(w1)
-    drawGraph2(w2)
-    drawGraph3(w3)
+    barGraph(w1, $graphsContainer)
+    barGraph(w2, $graphsContainer)
+    barGraph(w3, $graphsContainer)
   })
-
+  // setup tooltip
+  $(document.body).prepend('<div id = "time-tooltip"></div>')
+  spireDataStream.map(function (d) {
+    return {
+      from: d['metadata']['from'],
+      to:   d['metadata']['to']
+    }
+  }).onValue(function (range) {
+     var setTooltip = function (t) { 
+       $('#time-tooltip').html(t)
+     }
+     var mouseXToTime =  function (ev) {
+       var cx = ev.clientX
+       var t = map(cx, 0, window.innerWidth, range.from, range.to)
+       return new Date(t*1000)
+     }
+     // setup tooltip
+     var mouseMoves = Kefir.fromEvents($graphsContainer, 'mousemove')
+                           .map(mouseXToTime)
+                           .onValue(setTooltip)
+  })
 }
-
 $(document).on('ready', setup)
-
